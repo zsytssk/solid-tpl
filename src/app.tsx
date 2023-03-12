1a9063d449885f006112b10077f6c211a53f9277
shiyangzhang <shiyangzhang@whale.im>
2023-03-12 13:10:27 +0800

    save

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import { createSignal } from "solid-js";
 
 export const App = () => {
+  let ref: HTMLDivElement;
   const [count, setCount] = createSignal(0);
 
   return (
-    <div>
-      {count()}
+    <div ref={ref!}>
+      <div class="test1">{count()}</div>
+      <div>this is a test</div>
       <button onClick={() => setCount((old) => old + 1)}>click</button>
     </div>
   );
